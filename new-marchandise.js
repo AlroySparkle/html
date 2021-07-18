4100e38c4f6312a040476ec826ac5147bbf02a9e
Ali Alnasser <47244604+AlroySparkle@users.noreply.github.com>
2021-07-18 21:05:32 +0300

    day 10

--- new-marchandise.js
+++ new-marchandise.js
@@ -1,14 +1,19 @@
 var choicesArray = document.getElementsByClassName("game-choice");
 
 function nameGameImageContainer(imageSource, index) {
-    var mainImage = document.getElementsByClassName("new-game-image")[0];
-    mainImage.src = imageSource;
+    document.getElementById("new-game-image2").style.color = "rgba(0,0,0,0)";
     removeBorders();
     choicesArray[index].style.border = "#feed99 solid";
+    setTimeout(setImage(imageSource), 100);
 }
 
 function removeBorders() {
     for (var index = 0; index < choicesArray.length; index++) {
         choicesArray[index].style.border = "none";
     }
+}
+
+function setImage(imageSource) {
+    var mainImage = document.getElementsByClassName("new-game-image")[0];
+    mainImage.src = imageSource;
 }
